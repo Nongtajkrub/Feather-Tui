4df3de9113bade52da88d0ab27e3e107a197f834
nongtajkrub <sunanborthwic@gmail.com>
2025-03-02 15:45:27 +0700

    Renderer implmentation is mostly done

--- src/termui/renderer.rs
+++ src/termui/renderer.rs
@@ -1,28 +1,25 @@
-use crate::termui::container::Container;
-use crate::termui::components::{
-    header::Header,
-    option::Option,
-    text::Text,
-};
+use crate::termui::util::ansi::{self, ANSI_ESC_CURSOR_SHOW};
+use crate::termui::container;
+use crate::termui::components;
 
-const BG_CHAR: &'static str = " ";
-const NO_HEADER_ERRMSG: &'static str = "Renderer: Header is not set!";
+const BG_CHAR: &str = " ";
+const NO_HEADER_ERRMSG: &str = "Renderer: Header is not set!";
 
 struct Line {
-    ascii: std::option::Option<String>,
+    ansi: std::option::Option<String>,
     data: String,
 }
 
 impl Line {
     pub fn new(width: u16) -> Line {
         Line {
-            ascii: None,
+            ansi: None,
             data: BG_CHAR.repeat(width as usize)
         }
     }
 
-    pub fn set_ascii(&mut self, ascii: String) {
-        self.ascii = Some(ascii);
+    pub fn set_ansi(&mut self, ansi: String) {
+        self.ansi = Some(ansi);
     }
 
     pub fn edit(&mut self, data: &String, begin: u16) {
@@ -35,6 +32,21 @@ pub struct Renderer {
     height: u16,
     lines: Vec<Line>,
 }
+
+pub fn ready() {
+    print!(
+        "{}{}{}",
+        ansi::ANSI_ESC_CLEAR_TERM, 
+        ansi::ANSI_ESC_CURSOR_HOME, ansi::ANSI_ESC_CURSOR_HOME);
+}
+
+pub fn unready() {
+    print!(
+        "{}{}{}",
+        ansi::ANSI_ESC_CLEAR_TERM,
+        ansi::ANSI_ESC_CURSOR_HOME, ANSI_ESC_CURSOR_SHOW);
+}
+
 
 impl Renderer {
     pub fn new(width: u16, height: u16) -> Renderer {
@@ -55,27 +67,35 @@ impl Renderer {
         return lines;
     }
 
-    fn render_header(&mut self, header: &Header) {
+    fn render_header(&mut self, header: &components::header::Header) {
         let pos: u16 = 
             ((self.width as f32 - header.len() as f32) / 2.0).round() as u16; 
 
-        self.lines[0].edit(header.label(), pos);
+        let line = &mut self.lines[0];
+
+        line.edit(header.label(), pos);
+        line.set_ansi(String::from(ansi::ANSI_ESC_GREEN_B));
     }
 
-    fn render_options(&mut self, options: &Vec<Option>) {
+    fn render_options(&mut self, options: &Vec<components::option::Option>) {
         for option in options.iter() {
-            self.lines[option.line() as usize].edit(option.label(), 0);
+            let line = &mut self.lines[option.line() as usize];
+
+            line.edit(option.label(), 0);
+
+            if option.selc_on() {
+                line.set_ansi(String::from(ansi::ANSI_ESC_BLUE_B));
+            }
         }
     }
 
-    fn render_text(&mut self, texts: &Vec<Text>) {
+    fn render_text(&mut self, texts: &Vec<components::text::Text>) {
         for text in texts.iter() {
             self.lines[text.line() as usize].edit(text.label(), 0);
         }
     }
 
-    pub fn render(&mut self, container: &Container) {
-        // TODO: implement rendering options and texts
+    pub fn render(&mut self, container: &container::Container) {
         self.render_header(container.header().as_ref().expect(NO_HEADER_ERRMSG));
         self.render_options(container.options());
         self.render_text(container.texts());
@@ -83,8 +103,8 @@ impl Renderer {
 
     pub fn draw(&mut self) {
         for line in self.lines.iter() {
-            if let Some(ascii) = &line.ascii {
-                println!("{}{}", ascii, line.data);
+            if let Some(ansi) = &line.ansi {
+                println!("{}{}{}", ansi, line.data, ansi::ANSI_ESC_RESET);
             } else {
                 println!("{}", line.data)
             }
