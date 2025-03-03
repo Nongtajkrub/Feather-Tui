c3b247633ca60082e7b921fc9820eea2ac10c310
nongtajkrub <sunanborthwic@gmail.com>
2025-03-03 18:23:22 +0700

    test new library functions

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,57 @@ mod termui;
 
 use termui as tui;
 
-tui_cbk_new_callback_func!(func, arg, {
-    if let Some(num) = arg.downcast_ref::<u32>() {
-        println!("{}", num);
-    } else {
-        println!("Not a num");
-    }
+tui_trg_new_trigger_func!(up_trig_func, arg, {
+    return false;
+});
+
+tui_trg_new_trigger_func!(down_trig_func, arg, {
+    return false;
+});
+
+tui_trg_new_trigger_func!(selc_trig_func, arg, {
+    return true;
+});
+
+tui_cbk_new_callback_func!(callback_func1, arg, {
+    println!("Hello from 1!");
+});
+
+tui_cbk_new_callback_func!(callback_func2, arg, {
+    println!("Hello from 2!");
 });
 
 fn main() {
-    // let mut container = tui::con::Container::new();
+    let mut container = tui::container::Container::new();
+    let mut renderer = tui::ren::Renderer::new(40, 20);
+    let mut should_update = true;
 
-    let callback = tui::cbk::Callback::new(func, 12);
+    container.set_header(tui::cpn::hed::Header::new(String::from("Welcome!")));
 
-    callback.call();
+    container.add_option(
+        tui::cpn::opt::Option::new(String::from("Option1"),
+        tui::cbk::Callback::new(callback_func1, 0)));
+    container.add_option(
+        tui::cpn::opt::Option::new(String::from("Option2"),
+        tui::cbk::Callback::new(callback_func2, 0)));
 
-    /*
-    container.set_header(tui::cpn::hed::Header::new(String::from("Welcome")));
-
-    container.add_option(tui::cpn::opt::Option::new(String::from("Option1")));
-    container.add_option(tui::cpn::opt::Option::new(String::from("Option2")));
-
-    let mut renderer = tui::ren::Renderer::new(20, 10);
+    container.set_selector(tui::sel::Selector::new(
+            tui::trg::Trigger::new(up_trig_func, 0),
+            tui::trg::Trigger::new(down_trig_func, 0),
+            tui::trg::Trigger::new(selc_trig_func, 0)));
 
     tui::ren::ready();
 
-    renderer.clear();
-    renderer.render(&container);
-    renderer.draw();
+    for _ in 0..3 {
+        if should_update {
+            renderer.clear();
+            renderer.render(&container);
+            renderer.draw();
+        }
 
-    std::thread::sleep(std::time::Duration::from_secs(2));
-
-    renderer.clear();
-    renderer.render(&container);
-    renderer.draw();
-
-    std::thread::sleep(std::time::Duration::from_secs(2));
+        should_update = container.looper();
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 
     tui::ren::unready();
-    */
 }
