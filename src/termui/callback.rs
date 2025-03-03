e5bc4a42e84b4ce307bd7952b2a6fa1c7a3090b1
nongtajkrub <sunanborthwic@gmail.com>
2025-03-03 16:17:03 +0700

    Callback working.

--- src/termui/callback.rs
+++ src/termui/callback.rs
@@ -1,18 +1,18 @@
 #[macro_export]
 macro_rules! tui_cbk_new_callback_func {
     ($func_name:ident, $arg_name:ident, $body:block) => {
-        fn $func_name($arg_name: &dyn std::any::Any) $body
+        fn $func_name($arg_name: &Box<dyn std::any::Any>) $body
     };
 }
 
 pub struct Callback {
-    func: fn(arg: &dyn std::any::Any) -> (),
+    func: fn(arg: &Box<dyn std::any::Any>) -> (),
     arg: Box<dyn std::any::Any>,
 }
 
 impl Callback {
     pub fn new<T: 'static>(
-        func: fn(arg: &dyn std::any::Any),
+        func: fn(arg: &Box<dyn std::any::Any>),
         arg: T) -> Callback {
         Callback {
             func,
