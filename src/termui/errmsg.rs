2e728d41e1e5a1cfe1ad490c8a89d680b05545f9
nongtajkrub <sunanborthwic@gmail.com>
2025-03-03 23:05:53 +0700

    Implement Menu. (Not tested)

--- src/termui/errmsg.rs
+++ src/termui/errmsg.rs
@@ -1,2 +1,3 @@
 pub const NO_HEADER_ERRMSG: &str = "Container: Header is not set before using!";
 pub const NO_SELETOR_ERRMSG: &str = "Container: Selector is not set before using!";
+pub const NO_MAIN_MENU_ERRMSG: &str = "Menu: Main menu is not set before using!";
