12b72c0157da7ba6c8b7689713b3a952260705fd
nongtajkrub <sunanborthwic@gmail.com>
2025-03-04 11:45:30 +0700

    Rmove Menu.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ tui_trg_new_trigger_func!(down_trig_func, arg, {
 });
 
 tui_trg_new_trigger_func!(selc_trig_func, arg, {
-    return true;
+    return false;
 });
 
 tui_cbk_new_callback_func!(callback_func1, arg, {
@@ -30,13 +30,14 @@ fn main() {
     container.set_header(tui::cpn::hed::Header::new(String::from("Welcome!")));
 
     container.add_option(
-        tui::cpn::opt::Option::new(String::from("Option1"),
-        tui::cbk::Callback::new(callback_func1, 0)));
+        tui::cpn::opt::Option::new(
+            String::from("Option1"), tui::cbk::Callback::new(callback_func1, 0)));
     container.add_option(
-        tui::cpn::opt::Option::new(String::from("Option2"),
-        tui::cbk::Callback::new(callback_func2, 0)));
+        tui::cpn::opt::Option::new(
+            String::from("Option2"), tui::cbk::Callback::new(callback_func2, 0)));
 
-    container.set_selector(tui::sel::Selector::new(
+    container.set_selector(
+        tui::sel::Selector::new(
             tui::trg::Trigger::new(up_trig_func, 0),
             tui::trg::Trigger::new(down_trig_func, 0),
             tui::trg::Trigger::new(selc_trig_func, 0)));
