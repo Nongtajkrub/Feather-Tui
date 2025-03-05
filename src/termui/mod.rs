3083b796e7cc85e0ed894c67049851b36c4c376e
nongtajkrub <sunanborthwic@gmail.com>
2025-03-05 17:25:48 +0700

    Code style changeds

--- src/termui/mod.rs
+++ src/termui/mod.rs
@@ -6,15 +6,17 @@ pub mod container;
 pub mod renderer;
 pub mod input;
 pub mod menu;
-mod util;
-mod errmsg;
+
+mod     util;
+mod     errmsg;
 
 pub use components as cpn;
-pub use callback as cbk;
-pub use trigger as trg;
-pub use selector as sel;
-pub use container as con;
-pub use renderer as ren;
-pub use menu as mnu;
-pub use input as inp;
-use errmsg as emg;
+pub use callback   as cbk;
+pub use trigger    as trg;
+pub use selector   as sel;
+pub use container  as con;
+pub use renderer   as ren;
+pub use menu       as mnu;
+pub use input      as inp;
+
+use errmsg         as emg;
