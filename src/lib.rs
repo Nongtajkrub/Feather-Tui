6c8ff99e5e02e834756c4741849a9c13e08067b9
nongtajkrub <sunanborthwic@gmail.com>
2025-03-23 22:57:43 +0700

    Remove menu mod

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@ pub mod selector;
 pub mod container;
 pub mod renderer;
 pub mod input;
-pub mod menu;
 
 mod     util;
 mod     errmsg;
@@ -22,7 +21,6 @@ pub use trigger    as trg;
 pub use selector   as sel;
 pub use container  as con;
 pub use renderer   as ren;
-pub use menu       as mnu;
 pub use input      as inp;
 
 use errmsg         as emg;
