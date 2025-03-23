b59224ca0574caadc235df788f040a323b47d7bd
nongtajkrub <sunanborthwic@gmail.com>
2025-03-23 23:27:49 +0700

    Rename tui_cbk_new_callback_func! to cbk_new_callback_func!

--- src/callback.rs
+++ src/callback.rs
@@ -17,7 +17,7 @@
 /// });
 /// ```
 #[macro_export]
-macro_rules! tui_cbk_new_callback_func {
+macro_rules! cbk_new_callback_func {
     ($func_name:ident, $arg_name:ident, $body:block) => {
         fn $func_name($arg_name: &Box<dyn std::any::Any>) $body
     };
