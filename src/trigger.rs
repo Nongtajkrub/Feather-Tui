db412eb9bbd344258f15104eb7ad58639626702e
nongtajkrub <sunanborthwic@gmail.com>
2025-03-28 10:48:54 +0700

    Update documentation

--- src/trigger.rs
+++ src/trigger.rs
@@ -5,16 +5,21 @@ use std::any::Any;
 /// whether the condition is met.
 ///
 /// # Usage
-///
 /// Use for defining functions required to create a `Trigger` object.
+///
+/// # Parameters
+/// - `func_name`: An identifier (`ident`) representing the generated function name.
+/// - `arg_name`: An identifier (`ident`) representing the function argument name.
+/// - `body`: A block (`block`) containing the function implementation.
 /// 
 /// # Example
 /// ```
-/// // Define a trigger function that print the argument than evaluate whether the argument is 5
-/// tui_trg_new_trigger_func!(function_name, argument_name, {
-///     let number = *argument_name.downcast_ref::<u32>().unwrap();
+/// // Define a trigger function that print the argument than evaluate whether
+/// // the argument is 5
+/// trg_new_trigger_func!(func_name, arg, {
+///     let number = trg::cast_arg::<u32>(arg);
 ///     println!("{}", number);
-///     return number == 5;
+///     *number == 5
 /// });
 /// ```
 #[macro_export]
@@ -25,6 +30,33 @@ macro_rules! trg_new_trigger_func {
     };
 }
 
+/// Casts the argument of a trigger function to the specified type. For the time
+/// being this function will panic if the argument is not set or if the cast type
+/// is wrong.
+///
+/// # Parameters
+/// - `arg`: The argument of the trigger function.
+///
+/// # Notes
+/// - This function should only be use in a trigger function. 
+///
+/// # Usage
+/// use this function within a trigger function to cast the argument to the
+/// expected type.
+///
+/// # Example
+/// ```rust
+/// // A trigger function that take in a u32 an evaluate whether it is five.
+/// trg_new_trigger_func!(is_five, arg, {
+///     *trg::cast_arg::<u32>(arg) == 5
+/// });
+///
+/// trg::Trigger::new(is_five, 5u32).check(); // Evaluate to true
+/// trg::Trigger::new(is_five, 6u32).check(); // Evaluate to false
+///                                           
+/// trg::Trigger::new(is_five, "String").check(); // Panic
+/// trg::Trigger::no_arg(is_five).check();        // Panic
+/// ```
 #[inline]
 pub fn cast_arg<T>(arg: &Option<Box<dyn Any>>) -> &T 
 where
