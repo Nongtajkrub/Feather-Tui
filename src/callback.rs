825462e5a4f7be6ecf43425588a354bad8035f62
nongtajkrub <sunanborthwic@gmail.com>
2025-03-28 11:32:06 +0700

    Fix bug

--- src/callback.rs
+++ src/callback.rs
@@ -26,7 +26,9 @@ use crate::err::FtuiResult;
 #[macro_export]
 macro_rules! cbk_new_callback_func {
     ($func_name:ident, $arg_name:ident, $body:block) => {
-        fn $func_name($arg_name: &Option<Box<dyn std::any::Any>>) -> err::FtuiResult<()> $body
+        fn $func_name(
+            $arg_name: &Option<Box<dyn std::any::Any>>
+        ) -> feather_tui::err::FtuiResult<()> $body
     };
 }
 
