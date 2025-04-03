f5cd8ab4601589f5c2f2b42e5fb3b63aae231ed3
nongtajkrub <sunanborthwic@gmail.com>
2025-04-03 15:27:01 +0700

    Improve code quality.

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,4 @@
-use crate::{con, cpn, error::{FtuiError, FtuiResult}, util::ansi};
+use crate::{components::header, con, cpn, error::{FtuiError, FtuiResult}, util::ansi};
 
 const BG_CHAR: &str = " ";
 
@@ -18,14 +18,22 @@ impl Line {
         }
     }
 
+    #[inline]
     fn make_empty_line(width: u16) -> String {
         BG_CHAR.repeat(width as usize)
     }
 
+    #[inline]
     pub fn add_ansi(&mut self, value: &'static str) {
         self.ansi.push(value);
     }
 
+    #[inline]
+    pub fn add_ansi_many(&mut self, value: &Vec<&'static str>) {
+        value.iter().for_each(|ansi| self.ansi.push(ansi));
+    }
+
+    #[inline]
     pub fn edit(&mut self, data: &String, begin: u16) {
         self.data.replace_range(begin as usize..data.len() + begin as usize, data);
     }
@@ -138,28 +146,21 @@ impl Renderer {
         }
     }
 
+    #[inline]
     fn make_lines(width: u16, height: u16) -> Vec<Line> {
-        let mut lines: Vec<Line> = Vec::with_capacity(height as usize);
-
-        for _ in 0..height {
-            lines.push(Line::new(width));
-        }
-
-        return lines;
+        (0..height).map(|_| Line::new(width)).collect()
     }
 
     fn render_header(&mut self, header: &cpn::hed::Header) {
         let pos: u16 = 
             ((self.width as f32 - header.len() as f32) / 2.0).round() as u16; 
 
-        let line = &mut self.lines[0];
-
-        line.edit(header.label(), pos);
-        line.add_ansi(ansi::ESC_GREEN_B);
+        self.lines[0].edit(header.label(), pos);
+        self.lines[0].add_ansi(ansi::ESC_GREEN_B);
     }
 
-    fn render_options(&mut self, options: &Vec<cpn::opt::Option>) {
-        for option in options.iter() {
+    fn render_options(&mut self, options: &[cpn::opt::Option]) {
+        for option in options {
             let line = &mut self.lines[option.line() as usize];
 
             line.edit(option.label(), 0);
@@ -173,7 +174,7 @@ impl Renderer {
     fn resolve_text_pos(&self, text: &mut cpn::txt::Text) {
         // x pos
         if text.flags().contains(cpn::txt::TextFlags::ALIGN_MIDDLE) {
-            text.set_pos(((self.width as f32 - text.len() as f32) / 2.0) .round() as u16);
+            text.set_pos(((self.width as f32 - text.len() as f32) / 2.0).round() as u16);
         } else if text.flags().contains(cpn::txt::TextFlags::ALIGN_RIGHT) {
             text.set_pos(self.width - text.len() as u16);
         } else {
@@ -189,7 +190,7 @@ impl Renderer {
         text.set_pos_resolve(true);
     }
 
-    fn render_text(&mut self, texts: &mut Vec<cpn::txt::Text>) {
+    fn render_text(&mut self, texts: &mut [cpn::txt::Text]) {
         for text in texts.iter_mut() {
             if !text.pos_resolve() {
                 self.resolve_text_pos(text);
@@ -199,7 +200,7 @@ impl Renderer {
 
             line.edit(text.label(), text.pos());
             line.add_ansi(text.color());
-            text.styles().iter().for_each(|ansi| line.add_ansi(&ansi));
+            line.add_ansi_many(text.styles());
         }
     }
 
@@ -299,10 +300,9 @@ impl Renderer {
     ///     renderer.draw();
     /// }
     /// ```
+    #[inline]
     pub fn clear(&mut self) {
-        for line in self.lines.iter_mut() {
-            line.clear();
-        }
+        self.lines.iter_mut().for_each(|line| line.clear());
     }
 
     /// Executes a full rendering cycle in a single method call. This method 
