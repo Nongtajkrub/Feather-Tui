e36a8e603ffaf6dee6af6256e4d19aa6a056bb18
nongtajkrub <sunanborthwic@gmail.com>
2025-04-09 20:58:52 +0700

    Add Seperator module stuff to component module.

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -4,11 +4,14 @@ pub mod header;
 pub mod option;
 /// A UI component representing a text element in a `Container`.
 pub mod text;
+pub mod seperator;
 
-pub use header as hed;
-pub use option as opt;
-pub use text   as txt;
+pub use header    as hed;
+pub use option    as opt;
+pub use text      as txt;
+pub use seperator as sep;
 
 pub use hed::Header;
 pub use opt::Option;
 pub use txt::{Text, TextFlags};
+pub use sep::{Seperator, SeperatorStyle};
