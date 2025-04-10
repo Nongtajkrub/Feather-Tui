4455682127eb2c3126d38e23c32842f5711ac9fb
nongtajkrub <sunanborthwic@gmail.com>
2025-04-10 16:29:40 +0700

    Fully documented seperator component module.

--- src/components/seperator.rs
+++ src/components/seperator.rs
@@ -1,13 +1,60 @@
+/// An `enum` representing all possible styles for a `Separator` component.
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SeperatorStyle {
+    /// A solid block line.
+    ///
+    /// `███████████████████████████`
     Solid,
+
+    /// A medium-thickness line.
+    ///
+    /// `━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
     Medium,
+
+    /// A thin line.  
+    ///   
+    /// `──────────────────────────────`
     Thin,
+
+    /// A double line.
+    ///
+    /// `══════════════════════════════`
     Double,
+
+    /// A custom character for the separator. Example using the `+` character:
+    ///
+    /// `++++++++++++++++++++++++++++++`
     Custom(char),
 }
 
+/// A UI component that acts as a separator typically a horizontal line.
+/// `Separator` components are displayed in the order they are added to a
+/// `Container`. Each `Separator` can have a different style, specified using
+/// the `SeparatorStyle` enum. There are two types of separators available:
+/// normal and dotted.
+///
+/// # Notes
+/// - A normal separator looks like this: `-------`
+/// - A dotted separator looks like this: `- - - -`
+///
+/// # Usage
+/// The `Separator` component is useful for dividing sections in your terminal UI.
+///
+/// # Example
+/// ```rust
+/// use feather_tui as tui;
+///
+/// // Create a normal separator with a solid style.
+/// let separator = tui::cpn::Separator::normal(tui::cpn::SeparatorStyle::Solid);
+///
+/// // Create a dotted separator with a solid style.
+/// let separator = tui::cpn::Separator::dotted(tui::cpn::SeparatorStyle::Solid);
+///
+/// // Add the separator to a container.
+/// let mut container = tui::con::Container::new();
+/// container.add_separator(separator);
+/// ```
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Seperator {
     line: u16,
@@ -16,6 +63,22 @@ pub struct Seperator {
 }
 
 impl Seperator {
+    /// Creates a standard (non-dotted) `Seperator` with the given style.
+    ///
+    /// # Parameters
+    /// - `style`: The visual style of the separator, specified as a `SeperatorStyle`.
+    ///
+    /// # Returns
+    /// `Seperator`: A new `Seperator` instance.
+    ///
+    /// # Example
+    /// ```rust
+    /// use feather_tui as tui;
+    ///
+    /// // Create a normal separator with a solid style.
+    /// let separator =
+    ///     tui::cpn::Seperator::normal(tui::cpn::sep::SeperatorStyle::Solid);
+    /// ```
     pub fn normal(style: SeperatorStyle) -> Self {
         Seperator {
             line: 0,
@@ -24,6 +87,22 @@ impl Seperator {
         }
     }
 
+    /// Creates a dotted `Seperator` with the given style.
+    ///
+    /// # Parameters
+    /// - `style`: The visual style of the separator, specified as a `SeperatorStyle`.
+    ///
+    /// # Returns
+    /// `Seperator`: A new `Seperator` instance.
+    ///
+    /// # Example
+    /// ```rust
+    /// use feather_tui as tui;
+    ///
+    /// // Create a dotted separator with a solid style.
+    /// let separator =
+    ///     tui::cpn::Seperator::dotted(tui::cpn::sep::SeperatorStyle::Solid);
+    /// ```
     pub fn dotted(style: SeperatorStyle) -> Self {
         Seperator {
             line: 0,
