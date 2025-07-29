f4fd83b163eac4f762f17170b9b536a4eec03b43
nongtajkrub <sunanborthwic@gmail.com>
2025-07-29 22:12:02 +0700

    Fully document the message module.

--- src/container/message.rs
+++ src/container/message.rs
@@ -4,11 +4,16 @@ pub(crate) const MSG_INFO_ANSI: [&'static str; 2] = [ansi::ESC_WHITE_B, ansi::ES
 pub(crate) const MSG_WARN_ANSI: [&'static str; 1] = [ansi::ESC_YELLOW_B];
 pub(crate) const MSG_ERRO_ANSI: [&'static str; 2] = [ansi::ESC_RED_B, ansi::ESC_BOLD];
 
+/// Represents the visual style of a `Message`, typically used to convey different
+/// levels of importance or severity.
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum MessageStyle {
+    /// Informational message: white background with black foreground.
     Info,
+    /// Warning message: yellow background with white foreground.
     Warning,
+    /// Error message: red background with white foreground and bold text.
     Error,
 }
 
@@ -22,6 +27,12 @@ impl MessageStyle {
     }
 }
 
+/// A specialized variant of `Container` used to display a centered message on a
+/// `Renderer`. The appearance of the message is defined by the `MessageStyle` enum.
+/// 
+/// # Usage
+/// Use this to present informational messages, warnings, or errors to the user in
+/// a visually distinct way.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Message {
     message: String,
@@ -29,6 +40,19 @@ pub struct Message {
 }
 
 impl Message {
+    /// Creates a new `Message` with the given content and style.
+    ///
+    /// # Parameters
+    /// - `message`: A type that impl `ToString`, representing the message content.
+    /// - `style`: A `MessageStyle` indicating how the message should be displayed.
+    ///
+    /// # Returns
+    /// A new `Message` instance with the specified content and style.
+    ///
+    /// # Example
+    /// ```rust
+    /// let _ = Message::new("Information!", MessageStyle::Info);
+    /// ```
     pub fn new(message: impl ToString, style: MessageStyle) -> Self {
         Self {
             message: message.to_string(),
