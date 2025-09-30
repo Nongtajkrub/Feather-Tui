49d40f35ad9bcb569c0dfd892f460b3960707744
Nongtajkrub <sunanborthwic@gmail.com>
2025-09-30 19:01:33 +0700

    Imrpove modules importhing format in modules files.

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,15 +1,15 @@
 /// A UI component representing an interactive option in a `Container`. 
 pub(crate) mod option;
-/// A UI component representing a text element in a `Container`.
-pub(crate) mod text;
-/// A UI component that acts as a separator typically a horizontal line.
-pub(crate) mod seperator;
-
-pub(crate) use seperator::Separator;
-
 pub use option::Option;
 pub use option::OptionsManager;
+
+/// A UI component representing a text element in a `Container`.
+pub(crate) mod text;
 pub use text::Text;
 pub use text::TextFlags;
 pub use text::TextsManager;
+
+/// A UI component that acts as a separator typically a horizontal line.
+pub(crate) mod seperator;
+pub(crate) use seperator::Separator;
 pub use seperator::SeparatorStyle;
--- src/containers/mod.rs
+++ src/containers/mod.rs
@@ -1,13 +1,15 @@
 mod container;
-mod list;
-mod document;
-mod message;
-
 pub use container::Container;
 pub use container::ContainerBuilder;
+
+mod list;
 pub use list::List;
 pub use list::ListBuilder;
+
+mod document;
 pub use document::Document;
 pub use document::DocumentBuilder;
+
+mod message;
 pub use message::Message;
 pub use message::MessageStyle;
